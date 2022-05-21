f74bdfdaf63fa78712da17c6b22603ff8857736b
kuro_ye <erhanaket77@gmail.com>
2022-05-21 15:07:07 +0600

    给夏特克教编程

---
+++ main.py
@@ -0,0 +1,13 @@
+# people = 'Shattyk'
+# is_beauty = False
+#
+# if is_beauty == True:
+#     print(people+'是美女')
+# else:
+#     print('不是美女')
+
+for i in range(10):
+    if i % 2 == 0:
+        print('耶尔汗真帅'+str(i))
+    elif i % 2 == 1:
+        print('夏特克真美'+str(i))
