2d1daff46cc31025d57c855bd278e1c80b8d63e2
kuro_ye <erhanaket77@gmail.com>
2022-05-22 17:40:02 +0600

    learn while

---
+++ basic/while.py
@@ -0,0 +1,29 @@
+calculation_to_unit = 24
+name_of_unit = 'hours'
+
+def days_to_units(number_of_days):
+    return f'{number_of_days} days are {number_of_days * calculation_to_unit} {name_of_unit}'
+
+
+def validate_and_execute():
+    try:
+
+        input_value_number = int(input_value)
+        if input_value_number > 0:
+            calculated_value = days_to_units(int(input_value))
+            print(calculated_value)
+        elif input_value_number == 0:
+            print('You entered a 0, enter a valid value')
+        else:
+            print('You entered a negative number')
+
+    except ValueError:
+        print('Your input isn\'t a number. Don\'t ruin my program')
+
+input_value = ''
+while input_value != 'exit':
+    input_value = input('Hey, enter some value here\n')
+    # input() the value always be string type
+    if input_value != 'exit':
+        validate_and_execute()
+
